716bc9308e18329c49954c0a26867b1f00cdfa22
Erwanlbv <erwan.le-blevec@polytechnique.edu>
2024-03-29 09:05:33 +0100

    remove old scripts files for source & simulation

--- Simulation/sh_files/wcsim_12_5.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-module add Analysis/root/6.24.06
-module add Modelisation/geant4/10.03.p03
-
-export bin_path=/sps/t2k/bquilain/HK/Reconstruction/official_2023/Gonzalo/WCSim-home/WCSim_build/mydir/bin
-
-source ${bin_path}/this_wcsim.sh
