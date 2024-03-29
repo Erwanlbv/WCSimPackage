39f89050ed94097673f498fc746ee35eeb8f8451
Erwanlbv <erwan.le-blevec@polytechnique.edu>
2024-03-29 09:12:54 +0100

    create 12_8 folder

---
+++ Tools_WCSim_12_8/sh_files/root6_wcsim12_8.sh
@@ -0,0 +1,16 @@
+#!/bin/zsh
+
+# This file contains the minimum configuration 
+# to execute wcsim_root_to.C binary
+
+
+# Only root is needed
+module add Analysis/root/6.24.06
+
+# Set wcsim path
+wcsim_bin_path=/sps/t2k/bquilain/HK/Reconstruction/official_2023/Gonzalo/WCSim-1.12.8/WCSim_build/mydir/bin
+source ${wcsim_bin_path}/this_wcsim.sh
+
+export convert_bin=/sps/t2k/eleblevec/WCSimPackage/WCSimTools_12_8/bin/wcsimroot_to_root
+
+# Informative display
